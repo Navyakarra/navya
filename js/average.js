cf569c7fa86d31c316c80edad6042ad8bcc72f72
Navyareddy <navyareddykarra2003@gmail.com>
2025-03-11 10:01:33 -0500

    first commit

---
+++ js/average.js
@@ -0,0 +1,6 @@
+const age=[20,21,23]
+const sum=age[0]+age[1]+age[2]
+console.log("sum of the age",sum)
+const totalnumbers=3
+const average=sum/totalnumbers
+console.log ("average",average)
---
+++ js/script1.js
@@ -0,0 +1,14 @@
+let myfriends=['default']
+function adddmyfriends(friend)
+{
+    myfriends.unshift(friend)
+    console.log(myfriends)
+}
+for(leti=onabort;i<10;i++)
+{
+    console.log(i)
+    myfriends.push(i)
+}
+console.log(myfriends)
+
+
