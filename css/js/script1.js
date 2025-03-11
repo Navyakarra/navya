f35259f3d78392b49f66cfaf8327e39c5159dd09
Navyareddy <navyareddykarra2003@gmail.com>
2025-03-11 02:54:35 -0500

    first commit

---
+++ css/js/script1.js
@@ -0,0 +1,12 @@
+let myfriends=['default']
+function adddmyfriends(friend)
+{
+    myfriends.unshift(friend)
+    console.log(myfriends)
+}
+for(leti=onabort;i<10;i++)
+{
+    console.log(i)
+    myfriends.push(i)
+}
+console.log(myfriends)
